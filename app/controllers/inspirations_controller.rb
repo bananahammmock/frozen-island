2a7c9b7f327b5dbf82f261192b25f08d0ae0e2e2
bananahammmock <bananahammock2@gmail.com>
2013-12-09 23:10:11 -0800

    sdjbwrg

--- app/controllers/inspirations_controller.rb
+++ app/controllers/inspirations_controller.rb
@@ -10,7 +10,7 @@ class InspirationsController < ApplicationController
 		@insp.theme_id = 6
 		@insp.save!
 		flash[:notice] = "You have added #{@insp.quote}"
-		redirect_to root_url
+		redirect_to "/themes/6"
 	end
 	def show
 		@insp = Inspiration.find(params[:id])
@@ -32,9 +32,17 @@ class InspirationsController < ApplicationController
 		redirect_to "/inspirations/#{@y}"
 		
 	end
+	def destroy
+		@theme = Theme.find(6)
+		@inspiration = Inspiration.find(params[:id])
+		@inspiration.destroy
+		# @subscriptions = Subscription.where(:user_id => current_user.id, :theme_id => @theme.id)
+		# @subscriptions.destroy_all
+		redirect_to "/themes/6"
+	end
 private
 	
 	def inspiration_params
-		params.require(:inspiration).permit(:quote, :status)
+		params.require(:inspiration).permit(:quote, :status, :theme_id)
 	end
 end
